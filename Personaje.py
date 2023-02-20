47b6b520d648a39749fb1c3869de9abfe6ffa598
JonaRBL <121039246@upq.edu.mx>
2023-02-20 16:59:50 -0600

    Se agregaron los métodos de personaje

--- Personaje.py
+++ Personaje.py
@@ -4,3 +4,19 @@ class Personaje:
     especie = "Humano"
     nombre = "Master Chief"
     altura = "2.70"
+    
+    #metodos del personaje
+    
+    def correr(self, status):
+        if(status):
+            print("El personaje " + self.nombre + " esta corriendo")
+        else:
+            print("El personaje " + self.nombre + " se detuvo")
+    
+    def lanzarGranadas(self):
+        print("el personaje " + self.nombre + " lanzo una granada")
+    
+    def recargarArma(self, municiones):
+        cargador = 10
+        cargador = cargador + municiones
+        print("el arma tiene " + cargador + " balas")
