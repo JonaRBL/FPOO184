bc55634f4f4604c58c7c092c5b584e593e251261
JonaRBL <121039246@upq.edu.mx>
2023-03-01 16:21:22 -0600

    Tkinter: Funciones con botones

--- tkinther/practica_11.py
+++ tkinther/practica_11.py
@@ -1,7 +1,22 @@
 from tkinter import Tk,Button,Frame, messagebox
 
+
+#5.Declaramos funcion para los mensajes
 def mostrarMensaje():
-    messagebox.showinfo("¡¡AVISO!!", "Presionaste el boton azul")    
+    messagebox.showinfo("¡¡AVISO!!", "Presionaste el boton azul")
+    messagebox.showerror("ERROR", "Todo fallo con exito :|")
+    messagebox.askokcancel("Pregunta", "¿El jugó con tu corazon?")
+    messagebox.askquestion(":0", "¿soportas panzona?")
+    messagebox.askretrycancel("veremos", "o no puedes?")
+    messagebox.askyesno("cariñosos", "¿vamos a los cariños?")
+    messagebox.askyesnocancel("viaje", "pueblito magico o que?")
+    #investigar hacer input a traves del mensaje con caja de texto para colocar respuesta
+    
+#6.Funcion para agregar botones
+def agregarBoton():
+    botonVerde.config(text = "+", bg = "green", fg = "white", )
+    botonNuevo = Button(seccion3, text = "Nuevo")
+    botonNuevo.pack()
 
 #1.Instanciamos el objeto ventana
 ventana = Tk()
@@ -19,6 +34,7 @@ seccion3 = Frame(ventana, bg = "blue")
 seccion3.pack(expand = True, fill = 'both') 
 
 #3.Agregamos botones
+#4.Posicionamiento de elementos(place,grid,pack)
 botonAzul = Button(seccion1, text = "Boton Azul", fg = "blue", command = mostrarMensaje)
 botonAzul.place(x=60, y=60)
 
@@ -28,7 +44,7 @@ botonNegro.grid(row = 0,column = 0)
 botonAmarillo = Button(seccion2, text = "Boton Amarillo", bg = "#ffff4d")
 botonAmarillo.grid(row = 1, column = 1)
 
-botonVerde = Button(seccion3, text = "Boton Verde", bg ="green")
+botonVerde = Button(seccion3, text = "Boton Verde", fg ="black", command = agregarBoton)
 botonVerde.configure(height=2,width=10)
 botonVerde.pack(side="left")
 
